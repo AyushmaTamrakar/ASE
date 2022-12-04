3b8b106cf5ad8663ffc6d1a5655e0806acbf057a
Ayushma Tamrakar <tayushma20@tbc.edu.np>
2022-12-04 21:27:09 +0545

    Created class Shape

---
+++ Component1/Shape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Component1
+{
+    internal class Shape
+    {
+        private Color c;
+        private int x, y;
+        private bool fill;
+
+        public Shape()
+        {
+            c = Color.Black;
+            x = y = 0;
+
+        }
+        public Shape(Color c, int x, int y, bool fill)
+        {
+            this.c = c;
+            this.x = x;
+            this.y = y;
+            this.fill = fill;
+        }
+    }
+}
