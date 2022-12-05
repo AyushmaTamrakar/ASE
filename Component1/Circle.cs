c5436887e429eae234f401da69721e53fb578b8e
Ayushma Tamrakar <tayushma20@tbc.edu.np>
2022-12-05 21:49:35 +0545

    Added Circle class

---
+++ Component1/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Component1
+{
+    internal class Circle: Shape
+    {
+
+
+
+            int radius;
+
+            public Circle() : base()
+            {
+
+            }
+       
+            public Circle(Color color, bool fillshape, int x, int y, int radius) : base(color, x, y, fillshape)
+            {
+                this.radius = radius;
+            }
+
+        
+        
+        
+    }
+
+
+}
+
+
